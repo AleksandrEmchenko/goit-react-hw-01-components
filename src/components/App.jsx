7dec1eb737dbb6743158e756f76c9902958be49d
Aleksandr Yemchenko <aleksandremchenko@gmail.com>
2023-04-27 19:35:33 +0300

    add first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import { Product } from "./Product";
+import user from "../components/Profile/user.json";
+import { Profile } from "./Profile/Profile";
+
+export const App = () => (
+  <div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  </div>
+);
